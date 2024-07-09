6b970a05db982e5535dbe30cf5436ea99dd70950
Dan Ngo <danngo51@gmail.com>
2024-07-09 23:19:27 +0200

    changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -280,6 +280,7 @@ function Get-Theme {
             return
         }
     } else {
+        Write-Host "URL theme"
         oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
         Invoke-Expression (& { (zoxide init powershell | Out-String) })
     }
