7b90d23703e90785f6e4b3711ffd7793f635048c
Dan Ngo <danngo51@gmail.com>
2024-07-10 15:27:29 +0200

    changes

--- profile.ps1
+++ profile.ps1
@@ -17,3 +17,25 @@ function finder {
 function chrome {
   ii "C:\Program Files\Google\Chrome\Application\chrome.exe"
 }
+
+function mklink {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Link,
+        [Parameter(Mandatory=$true)]
+        [string]$Target,
+        [switch]$D,
+        [switch]$Directory
+    )
+
+    if ($D -or $Directory) {
+        cmd /c mklink /D "`"$Link`"" "`"$Target`""
+    } elseif ($PSCmdlet.MyInvocation.BoundParameters.Count -eq 0) {
+        Write-Host "Usage:"
+        Write-Host "mklink <Link> <Target>"
+        Write-Host "mklink -D <Link> <Target>"
+        Write-Host "mklink -Directory <Link> <Target>"
+    } else {
+        cmd /c mklink "`"$Link`"" "`"$Target`""
+    }
+}
